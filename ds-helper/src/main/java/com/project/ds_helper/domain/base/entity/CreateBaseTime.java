cd30c01a76e06cb86c370b242cf642e720b0ecf9
leeyunho <dldbsgh00005@gmail.com>
2025-07-31 14:11:11 +0900

    update : createBaseTime

---
+++ ds-helper/src/main/java/com/project/ds_helper/domain/base/entity/CreateBaseTime.java
@@ -0,0 +1,27 @@
+package com.project.ds_helper.domain.base.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+public class CreateBaseTime {
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
