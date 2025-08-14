78ed88fd5474d533523f1942b3790236c6e8edef
leeyunho <dldbsgh00005@gmail.com>
2025-08-14 16:54:22 +0900

    udate : kakaocontroller

--- ds-helper/src/main/java/com/project/ds_helper/domain/user/controller/KakaoAuthController.java
+++ ds-helper/src/main/java/com/project/ds_helper/domain/user/controller/KakaoAuthController.java
@@ -30,7 +30,7 @@ public class KakaoAuthController {
 
     @Tag(name = "유저")
     @Operation(summary = "카카오 회원가입 & 로그인 API")
-    @GetMapping("/join")
+    @GetMapping("/login")
     public ResponseEntity<?> JoinWithKakaoOauthToken(@RequestParam("code") String kakaoAuthToken,
                                                            HttpServletResponse httpServletResponse) throws InterruptedException {
         return ResponseEntity.ok(kakaoOauthService.JoinWithKakaoOauthToken(kakaoAuthToken, httpServletResponse));
